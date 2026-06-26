29901929e7e35290ef7f99bc304568806e54ecd7
Aaron Ang <aaron.boxuan.ang@gmail.com>
2026-06-26 11:14:34 +1000

    initial

---
+++ Main.py
@@ -0,0 +1,19 @@
+import customtkinter
+THEMES = {
+    "dark": {
+        "background": "#1A1A2E",
+        "text": "#F0F4F8",
+        "cards": "#232946",
+        "buttons": "#8A84E0",
+        "guidance": "#DFA59A",
+        "success": "#7DD3C0"
+    },
+    "light": {
+        "background": "#D6DDF1",
+        "text": "#1E293B",
+        "cards": "#F5F5FF",
+        "buttons": "#6B64C9",
+        "guidance": "#B87D6E",
+        "success": "#4A9B94"
+    }
+}
