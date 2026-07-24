99d010f21ba24eaba6e9ed2c49fe9c60791f69a9
MoldyCat <aaron.boxuan.ang@gmail.com>
2026-07-24 10:01:49 +1000

    initial classes setup

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import customtkinter
+import json
 THEMES = {
     "dark": {
         "background": "#1A1A2E",
@@ -17,3 +18,27 @@ THEMES = {
         "success": "#4A9B94"
     }
 }
+factors_dict = {}
+
+class Task():
+    def __init__(self, task_id, name, tags_list, start_time, due_date, duration, factor_urgency, factor_ambition, status, description):
+        pass
+class Routine():
+    def __init__(self, name, desc, tags_list, daily_status, recurrence_pattern, description):
+        pass
+
+class Fixed_Routine(Routine):
+    def __init__(self, start_time, end_time):
+        pass
+class Flexible_Routine(Routine):
+    def __init__(self, min_duration, progress, last_reset_date):
+        pass
+class User_Profile():
+    def __init__(self, equilibrium_score, current_factor_ratings, target_goals, historical_data_log):
+        pass
+class Focus_Session():
+    def __init__(self, target_task_id, target_duration, total_elapsed_time, legitimate_pause_duration, distraction_pause_duration, session_outcome):
+        pass
+class Task_Manager():
+    def __init__(self, active_tasks, procrastinated_tasks, completed_tasks):
+        pass
